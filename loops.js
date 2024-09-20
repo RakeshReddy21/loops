5f5c4a4daf69b95263467f95f78420fd7b1cf828
RakeshReddy21 <reddykr21@gmail.com>
2024-09-20 20:28:12 +0530

    Loops

--- loops.js
+++ loops.js
@@ -1,117 +1,88 @@
-function demo(a,b){
-    console.log(a+b);
+for(i=1;i<=10;i++){
+    x=i**2;
+    console.log(x);
 }
-demo(23,45)
+
+console.log("Sum of Number from 1 to 20 which are divisible by 3 ");
 
 
-
-
-
-x=(a,b)=>{ return (a+b)}
-console.log(x(23,45))
-
-
-
-
-
-y = function(a,b){
-    return a+b
+let sum=0;
+for(i=1;i<=20;i++){
+    if(i%3==0){
+        sum=sum+i
+        // console.log(sum);
+    } 
 }
-console.log(y(23,45))
+console.log(sum);
+
+console.log("Cube of the each number from 1 to 15 ");
+
+for(i=1;i<=15;i++){
+    x=i**3;
+    console.log(x);
+}
+
+console.log("The numbers from 10 to 1 in reverse order");
+
+let a=10;
+do{
+    console.log(a);
+    a--;
+}while(a>0)
+
+console.log("The product of all numbers from 1 to 12");
+
+let y=1;
+let product=1;
+do{
+    // console.log(x);
+    y++;
+    product=product*y;
+}while(y<12)
+    console.log(product);
+
+console.log("The numbers from 20 to 1 in reverse order returning alternate number");
+
+let z=20;
+do{
+    console.log(z);
+    z-=2;
+}while(z>=1)
+    
+console.log("The numbers from 1 to 20 but skipping the numbers that divisible by 5");
 
 
-
-
-
-function table(x){
-    for(let i=1;i<=10;i++){
-        console.log(`${x}X${i}=${x*i}`);
+let f=0;
+// sum=0;
+while(f<20){
+    // console.log(f);
+    f++;
+    if(f%5!=0){
+        console.log(f);
     }
 }
-table(3); 
 
+console.log("The summ of all numbers from 1 to 30 that divisible by 4");
 
-
-
-
-a=(x)=>{
-    for(i=1;i<=10;i++){
-        console.log(x*i);
+let p=1;
+sum=0;
+while(p<30){
+    // console.log(p);
+    p++;
+    if(p%4==0){
+        sum=sum+p;
     }
 }
-a(3)
+console.log(sum);
 
+console.log("The numbers from 50 to 1 but returing only the numbers that divisible by 3");
 
-
-
-
-a=()=>{
-    let x="HTML "
-    let y="CSS "
-    let z="Javascript"
-    return x+y+z;
-}
-console.log(a())
-
-
-
-
-
-function fizz(x, y){
-    for(i=x;i<=y;i++){
-        if(i%2==0 && i%3==0){
-            console.log(i,"fizzbuzz");
-        }
-        else if(i%2==0){
-            console.log(i, "fizz");
-        }else if(i%3==0){
-            console.log(i,"buzz");
-        }
-        // console.log(i);
+let c=50;
+while(c>1){
+    // console.log(c);
+    --c;
+    if(c%3==0){
+        console.log(c);
     }
 }
-fizz(20,40) 
 
-
-
-
-
-b=()=>{
-    for(i=0;i<=20;i++){
-        if(i%2==0 && i%3==0){
-            console.log(i,"fizzbuzz");
-        }
-        else if(i%2==0){
-            console.log(i,"fizz")
-        }
-        else if(i%3==0){
-            console.log(i,"buzz")
-        }
-    }
-}
-b()
-
-
-
-
-
-c=()=>{
-    let str="JAVASCRIPT"
-    for(i=0;i<=str.length-1;i++){
-        console.log(str[i]);
-    }
-    return "reverse order"
-}
-c()
-
-
-
-
-
-c=function(){
-    let str="JAVASCRIPT"
-    for(i=str.length-1;i>=0;i--){
-        console.log(str[i]);
-    }
-}
-c()
